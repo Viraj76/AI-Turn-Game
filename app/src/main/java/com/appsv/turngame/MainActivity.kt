89e92836b9ae0763150e20710da7ce9bc729f0b5
Viraj76 <virajg.7680@gmail.com>
2024-07-21 12:41:47 +0530

    splash screen

--- app/src/main/java/com/appsv/turngame/MainActivity.kt
+++ app/src/main/java/com/appsv/turngame/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.appsv.turngame.ui.theme.TurnGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             TurnGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
